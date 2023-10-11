5bca10adfaefe64303294cdd96bc37665b8000f2
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-11 07:33:18 -0500

    Add function to run a game round

--- game.js
+++ game.js
@@ -17,4 +17,31 @@ function getComputerChoice(){
 
 // The user's choice will be compared against the computer's choice, and a winner will be declared
 
+function playRound(playerSelection, computerSelection){
+
+    // Convert player choice to consistent case...
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    // ...and check that it's Rock, Paper, or Scissors
+    if (
+        playerSelection !== "Rock" &&
+        playerSelection !== "Paper" &&
+        playerSelection !== "Scissors"){
+            alert("You must choose Rock, Paper, or Scissors!");
+    }
+
+    // Compare user choice with computer choice and return the result
+    if (playerSelection === computerSelection){
+        return "It's a draw!";
+    } else if (
+        playerSelection === "Rock" && computerSelection === "Scissors"||
+        playerSelection === "Paper" && computerSelection === "Rock" ||
+        playerSelection === "Scissors" && computerSelection === "Paper"){
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${playerSelection} loses to ${computerSelection}`
+    }
+}
+
+console.log(playRound());
 // The result will be output to the console
