0e51133dddb69efc846d394ce45defcf97444ddb
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-10 21:56:55 -0500

    Add function to generate computer choice

--- game.js
+++ game.js
@@ -2,6 +2,19 @@
 
 //The computer will determine a choice between rock, paper, and scissors, and that choice will be stored in a variable
 
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3) + 1;
+    
+    switch(choice) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors"
+    }
+}
+
 // The user's choice will be compared against the computer's choice, and a winner will be declared
 
 // The result will be output to the console
