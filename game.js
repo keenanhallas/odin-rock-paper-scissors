eba259ac310e36690af8f17ea6b40e281e17f50b
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-10 21:33:17 -0500

    Add comments breaking down the problem into sub-problems in game.js

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
-console.log("Test");
+// The user will be prompted for a choice between rock, paper, & scissors in the console, and that choice will be stored in a variable
+
+//The computer will determine a choice between rock, paper, and scissors, and that choice will be stored in a variable
+
+// The user's choice will be compared against the computer's choice, and a winner will be declared
+
+// The result will be output to the console
