41ad9d37ad9b54891ba4e78ebd22fd7323c39a96
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-13 00:08:07 -0500

    Add game function, refactor user input into separate function

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// The user will be prompted for a choice between rock, paper, & scissors in the console, and that choice will be stored in a variable
+// The player will be prompted for a choice between rock, paper, & scissors in the console, and that choice will be stored in a variable
 
 //The computer will determine a choice between rock, paper, and scissors, and that choice will be stored in a variable
 
@@ -15,30 +15,100 @@ function getComputerChoice(){
     }
 }
 
-// The user's choice will be compared against the computer's choice, and a winner will be declared
+function getPlayerChoice(){
+    let playerChoice;
 
-function playRound(playerSelection, computerSelection){
+    while(!playerChoice){
+        let answer;
 
-    // Convert player choice to consistent case...
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        // Prompt player for answer and don't continue without one
+        while(!answer){
+            answer = prompt("Please choose Rock, Paper, or Scissors:");
+        }
 
-    // ...and check that it's Rock, Paper, or Scissors
-    if (
-        playerSelection !== "Rock" &&
-        playerSelection !== "Paper" &&
-        playerSelection !== "Scissors"){
-            alert("You must choose Rock, Paper, or Scissors!");
-    }
+        // Convert answer to consistent case...
+        answer = answer.charAt(0).toUpperCase() + answer.slice(1).toLowerCase();
+        console.log(answer);
 
-    // Compare user choice with computer choice and return the result
-    if (playerSelection === computerSelection){
-        return "It's a draw!";
-    } else if (
-        playerSelection === "Rock" && computerSelection === "Scissors"||
-        playerSelection === "Paper" && computerSelection === "Rock" ||
-        playerSelection === "Scissors" && computerSelection === "Paper"){
-            return `You win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-        return `You lose! ${playerSelection} loses to ${computerSelection}`
+        // ...and check that it's Rock, Paper, or Scissors
+        if (
+            // If not, the user will have to type in another answer
+            answer !== "Rock" &&
+            answer !== "Paper" &&
+            answer !== "Scissors"){
+                alert("You must choose Rock, Paper, or Scissors!");
+        } else {
+            // If answer is acceptable, set playerChoice to answer and return it
+            playerChoice = answer;
+            return playerChoice;
+        }
     }
 }
+
+// The player's choice will be compared against the computer's choice, and a winner will be declared
+
+function playRound(playerChoice, computerChoice){
+    // Compare player choice with computer choice and return the result
+    if (playerChoice === computerChoice){
+        alert("It's a draw!");
+        return "Draw";
+    } else if (
+        playerChoice === "Rock" && computerChoice === "Scissors"||
+        playerChoice === "Paper" && computerChoice === "Rock" ||
+        playerChoice === "Scissors" && computerChoice === "Paper"){
+            alert(`You win this round! ${playerChoice} beats ${computerChoice}`);
+            return "Player";
+    } else {
+        alert(`You lose this round! ${playerChoice} loses to ${computerChoice}`);
+        return "Computer";
+    }
+}
+
+//A game function will handle the running of games
+function game(){
+    //GAME CONTEXT
+	//The player's score will be set to 0
+    let playerScore = 0;
+
+    //The computer's score will be set to 0
+    let computerScore = 0;
+
+    //The round count will be set to 0
+    let rounds = 0;
+
+    //5 rounds will be played
+    while(rounds < 5){
+        //ROUND CONTEXT
+		//A computer choice will be generated
+        let computerChoice = getComputerChoice();
+
+        //The player will be prompted for a choice
+        let playerChoice = getPlayerChoice();
+
+        //A round will be played using these choices, returning a winner
+        let winner = playRound(playerChoice, computerChoice);
+
+        //The appropriate score will be updated
+        if(winner === "Player"){
+            playerScore++;
+            //The round count will be updated
+            rounds++;
+        } else if(winner === "Computer") {
+            computerScore++;
+            //The round count will be updated
+            rounds++;
+        }
+        //The rounds count will not be incremented when a draw happens
+    }
+
+    //The scores and winner will be shown
+    alert(`
+    Player score: ${playerScore}
+    Computer score: ${computerScore}
+    ${playerScore > computerScore ?
+        "Player wins!" :
+        "Computer wins!"}
+    `);
+}
+
+game();
